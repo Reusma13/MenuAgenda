093438b6f02d5b9ab8af51203eaa1cfb9e74f0d0
Marc Vancea <marcvancea@MacBook-Pro-de-marc.local>
2024-02-01 11:43:10 +0100

    Parte ordenarAgenda hecha

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using Internal;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MenuAgenda
@@ -9,6 +11,23 @@ namespace MenuAgenda
         {
             MostrarMenu();
         }
-        
+        static void OrdenarAgenda()
+        {
+            var lineas = File.ReadLines("agenda.txt")
+                .Select(linea => new
+                {
+                    Datos = linea.Split(';'),
+                    Nombre = linea.Split(';')[0]
+                })
+                .OrderBy(usuario => usuario.Nombre)
+                .Select(usuario => string.Join(";", usuario.Datos))
+                .ToList();
+
+            File.WriteAllLines("agenda.txt", lineas);
+            Console.WriteLine("La agenda a sido ordenada.");
+            RecuperarUsuariSegons();
+        }
+
+
     }
 }
