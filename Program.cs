e942a9bfd3b7668dc92c9ef3dacbe21f4d42fc1f
Marc Vancea <marcvancea@MBP-de-marc.home>
2024-02-03 13:29:22 +0100

    Parte RecuperarUsuariString hecha

--- Program.cs
+++ Program.cs
@@ -55,4 +55,29 @@ namespace MenuAgenda
 
 
         }
+        static string RecuperarUsuari(string nomUsuari)
+        {
+            char trobarUsuari = 'S';
+            bool trobat;
+            while (trobarUsuari != 'N' && trobarUsuari != 'n')
+            {
+                var linea = File.ReadLines("agenda.txt")
+                    .Select(linea => linea.Split(';')[0]).ToList(); // Ponemos que esta separado por ; i que el nombre de usuario es el primero
+
+                trobat = linea.Contains(nomUsuari);
+
+                if (trobat)
+                {
+                    trobarUsuari = 'N';
+                }
+                else
+                {
+                    Console.Write("Usuari no trobat. Vols trobar un altre usuari? (S/N)");
+                    trobarUsuari = Convert.ToChar(Console.ReadLine());
+                }
+            }
+            return nomUsuari;
+        }
+
     }
+}
